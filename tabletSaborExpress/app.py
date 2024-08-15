2d54ade72f889a6aec0b606f7a2eca28f52988d3
Roberto Dias <104544845+underthedarkxx@users.noreply.github.com>
2024-08-15 00:49:43 +0000

    criando sabor express para rodar no tablet

---
+++ tabletSaborExpress/app.py
@@ -0,0 +1,125 @@
+import os
+
+restaurantes = [{'nome': 'praça', 'categoria':'Japonesa', 'ativo':False},
+ {'nome':'Pizza Suprema', 'categoria':'Italiana', 'ativo':True}, 
+ {'nome': 'Cantina', 'categoria':'Italiano', 'ativo':False}]              
+
+def exibir_nome_do_programa():
+    '''Exibe o nome do app'''
+    print("""
+        ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+        ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+        ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+        ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+        ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+        ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+    """)
+
+def exibir_opcoes():
+    '''Opções que o usuário pode escolher'''
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Alternar estado do restaurante')
+    print('4. Sair\n')
+
+def finalizar_app():
+    '''É responsavel por exibir o titulo finalizar app'''
+    exibir_subtitulo('Finalizar app')
+
+def voltar_ao_menu_principal():
+    '''Responsavel por voltar ao menu principal'''
+    input('\nDigite uma tecla para voltar ao menu ')
+    main()
+
+def opcao_invalida():
+    '''É responsavel por mostrar que a opção digitada é invalida'''
+    print('Opção inválida!\n')
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    '''Essa Função é responsavel por limpar o prompt e colocar o titulo com detalhes de * em volta do titulo'''
+    os.system('clear')
+    linha = '*' * (len(texto) + 4)
+    print (linha)
+    print(texto)
+    print(linha)
+    print()
+
+def cadastrar_novo_restaurante():
+    '''Essa Função é responsavel por cadastrar um novo restaurante
+    
+    Inputs:
+    -Nome do Restaurante
+    -Categoria
+
+    outputs:
+    -Adiciona um novo restaurante a lista de restaurantes
+
+    '''
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}:')
+    dados_do_restaurante = {'nome':nome_do_restaurante, 'categoria':categoria, 'ativo':False}
+    restaurantes.append(dados_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    '''Listando os restaurantes salvos com nome, categoria e estado'''
+    exibir_subtitulo('Listando restaurantes')
+
+    for restaurante in restaurantes:
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+        print(f'- {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}')
+
+    voltar_ao_menu_principal()
+
+def alternar_estado_restaurante():
+    '''É responsavel por alterar o estado atual do restaurante'''
+    exibir_subtitulo('Alternando estado do restaurante')
+    nome_restaurante = input('Digite o nome do restaurante que deseja alterar o estado: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante}foi desativado com sucesso'
+            print(mensagem)
+
+    if not restaurante_encontrado:
+        print('O restaurante não foi encontrado')
+
+    voltar_ao_menu_principal()
+
+def escolher_opcao():
+    '''Verifica qual opçào o usuario escolheu'''
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # opcao_escolhida = int(opcao_escolhida)
+
+        if opcao_escolhida == 1: 
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2: 
+            listar_restaurantes()
+        elif opcao_escolhida == 3: 
+            alternar_estado_restaurante()
+        elif opcao_escolhida == 4: 
+            finalizar_app()
+        else: 
+            opcao_invalida()
+    except:
+        opcao_invalida()
+
+def main():
+    '''Main do programa'''
+    os.system('clear')
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
