fe4bd422e92b98648995dc81addee9d3411ffe1b
underthedarkxx <roberto.diasfurtadoaraujo@gmail.com>
2024-08-16 15:08:01 -0300

    update desafios modulo 1

---
+++ OO-SaborExpress/Exercicios/exericicos.py
@@ -0,0 +1,38 @@
+class Restaurante:
+    nome=''
+    categoria = ''
+    ativo = False
+
+restaurante_praca = Restaurante()
+'''restaurante_praca.nome = 'Mama Mia' '''
+restaurante_praca.nome = 'Bistrô'
+restaurante_praca.categoria = 'Italiana'
+
+nome_do_restaurante = restaurante_praca.nome
+
+print(nome_do_restaurante)
+
+if restaurante_praca.ativo:
+    print('O restaurante está ativo')
+else:
+    print('O restaurante está inativo')
+
+categoria = Restaurante.categoria
+
+restaurante_pizza = Restaurante()
+restaurante_pizza.nome = 'Pizza Place'
+restaurante_pizza.categoria = 'Fast Food'
+restaurante_pizza.ativo = True
+
+if restaurante_pizza.categoria == 'Fast Food':
+    print('A categoria é Fast Food.')
+else:
+    print('A categoria não é Fast Food.')
+    
+if restaurante_pizza.ativo:
+    print('O restaurante está ativo')
+else:
+    print('O restaurante está inativo')
+
+print(f'Nome: {restaurante_praca.nome}, Categoria: {restaurante_praca.categoria}')
+print(f'Nome: {restaurante_pizza.nome}, Categoria: {restaurante_pizza.categoria}')
